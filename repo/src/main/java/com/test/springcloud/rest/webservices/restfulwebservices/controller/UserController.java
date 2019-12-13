67832463ee189836a20ad47846619fa2a15a6cdf
Ivan Florez <ivan.florez@globant.com>
2019-12-13 18:01:17 -0500

    Advance on Udemy Course, add Swagger documentation Throwback the Spring boot version to support the integration with swagger.

--- repo/src/main/java/com/test/springcloud/rest/webservices/restfulwebservices/controller/UserController.java
+++ repo/src/main/java/com/test/springcloud/rest/webservices/restfulwebservices/controller/UserController.java
@@ -7,10 +7,15 @@ import com.test.springcloud.rest.webservices.restfulwebservices.exception.UserNo
 import com.test.springcloud.rest.webservices.restfulwebservices.service.UserDaoService;
 import com.test.springcloud.rest.webservices.restfulwebservices.service.UserPostDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -33,15 +38,16 @@ public class UserController {
     }
 
     @GetMapping(path = "/users/{id}")
-    public EntityModel<User> retrieveUser(@PathVariable final Integer id) {
+    public Resource<User> retrieveUser(@PathVariable final Integer id) {
         Optional<User> optUser = userDaoService.findOne(id);
         if (!optUser.isPresent()) {
             throw new UserNotFoundException("Id: " + id);
         }
 
-        EntityModel<User> resource = new EntityModel<>(optUser.get());
-        WebMvcLinkBuilder linkTo = WebMvcLinkBuilder
-              .linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveAllUsers());
+        Resource<User> resource = new Resource<User>(optUser.get());
+        ControllerLinkBuilder linkTo =
+              ControllerLinkBuilder.linkTo(ControllerLinkBuilder
+                    .methodOn(this.getClass()).retrieveAllUsers());
 
         resource.add(linkTo.withRel("all-users"));
 
